059fd0a3052457d63f7312a2e043909a6bbda7e8
DiogoLuisFC <diogoluisfc@msn.com>
2022-09-19 11:44:21 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_file, redirect, flash
+from http.server import BaseHTTPRequestHandler
 from PyPDF2 import PdfReader, PdfFileWriter
 from werkzeug.utils import secure_filename
 from zipfile import ZipFile
